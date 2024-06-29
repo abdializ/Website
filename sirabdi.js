3eb20821b94aa6d06c93d7d8ac2f73f92cc75958
abdializ <113491278+abdializ@users.noreply.github.com>
2024-06-29 08:53:15 -0500

    Update sirabdi.js

--- sirabdi.js
+++ sirabdi.js
@@ -1,15 +1,32 @@
-const text = document.getElementById('text');
-const textContent = 'Under-Construction';
+const text = document.getElementById('p-text');
+const initialText = '';
+const texts = [
+    '在建中', // Chinese
+    'In costruzione', // Italian
+    'قيد الإنشاء', // Arabic
+    'Socodka dhismaha', // Somali
+    'Under Construction'  //English
+];
+let textIndex = 0;
 let index = 0;
 
 function type() {
-    if (index < textContent.length) {
-        text.innerHTML += textContent.charAt(index);
+    if (index < initialText.length) {
+        text.innerHTML += initialText.charAt(index);
         index++;
         setTimeout(type, 100);
+    } else {
+        setTimeout(changeText, 5000); // Start changing text after a short delay
     }
+}
+
+function changeText() {
+    text.innerHTML = texts[textIndex];
+    textIndex = (textIndex + 1) % texts.length;
+    setTimeout(changeText, 3000); // Change text every 3 seconds
 }
 
 document.addEventListener('DOMContentLoaded', (event) => {
     type();
 });
+
